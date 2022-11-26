e126840139071f204d4ff3d9e4eb0b654ef362fb
ismael-soler <ismaelsolerferreira@gmail.com>
2022-11-26 20:01:24 -0300

    commit

--- models/user.py
+++ models/user.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import Base, BaseModel
+from models.base_model import BaseModel, Base
+import sqlalchemy
 from sqlalchemy import Column, String
+import models
+import sqlalchemy
+from os import getenv
 from sqlalchemy.orm import relationship
-import os
-STRG = os.environ.get('HBNB_TYPE_STORAGE')
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    if STRG == "db":
-        __tablename__ = "users"
-        email = Column(String(128), nullable = False)
-        password = Column(String(128), nullable = False)
-        first_name = Column(String(128), nullable = False)
-        last_name = Column(String(128), nullable = False)
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user")
+    reviews = relationship("Review", backref="user")
