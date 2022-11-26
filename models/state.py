d5318760160de012eef56e1605471e4d7c804921
ismael-soler <ismaelsolerferreira@gmail.com>
2022-11-26 20:22:48 -0300

    commit

--- models/state.py
+++ models/state.py
@@ -1,33 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-import sqlalchemy
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey, Integer
 from sqlalchemy.orm import relationship
-from os import getenv
-from models.city import City
+import models
+import os
+STRG = os.environ.get('HBNB_TYPE_STORAGE')
+
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    if (getenv("HBNB_TYPE_STORAGE") == "db"):
-        cities = relationship("City", backref="state", cascade="all, \
-                              delete-orphan")
+    if STRG == 'db':
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade="all, delete, delete-orphan")
     else:
         name = ""
 
-    def __init__(self, *args, **kwargs):
-        """init state"""
-        super().__init__(*args, **kwargs)
-
-    if (getenv("HBNB_TYPE_STORAGE") != "db"):
         @property
         def cities(self):
-            """cities"""
-            from models import storage
-            stacit = []
-            for stat in storage.all(City).values():
-                if self.id == city.state_id:
-                    stacit.append(storage.all(City)[stat])
-            return stacit
+            """ Getter method """
+            from models.city import City
+            return [obj for obj in models.storage.all(City).values() if
+                    obj.state_id == self.id]
