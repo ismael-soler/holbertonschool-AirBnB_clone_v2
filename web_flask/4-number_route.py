dc13d9d2746f908232d59a6a1ede1a156c951ce5
ismael-soler <ismaelsolerferreira@gmail.com>
2022-11-29 21:22:10 -0300

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+if the route is /number/<n>,
+return <n> is a number.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python', defaults={'text': "is cool"}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text="is cool"):
+    return "Python " + text.replace('_', ' ')
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+        return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, port=5000)
